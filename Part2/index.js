e7de8acc66238a3f0efac058d3ea377a062f6abe
Ian Kaliszewski <ikalisz@yahoo.com>
2019-04-22 15:57:06 -0600

    finished cards and works

--- Part2/index.js
+++ Part2/index.js
@@ -1,3 +1,23 @@
 let cardId = document.getElementById('cardId')
 let cardType = document.getElementById('cardType')
 
+function resetCards() {
+    let diamond = document.getElementById('diamond')
+    let spade = document.getElementById('spade')
+    let heart = document.getElementById('heart')
+    let club = document.getElementById('club')
+    diamond.style.color = 'grey'
+    spade.style.color = 'grey'
+    heart.style.color = 'grey'
+    club.style.color = 'grey'
+}
+
+function setCard() {
+    let card = document.getElementById(cardId.value)
+    console.log(card)
+    if (cardId.value === 'heart' || cardId.value === 'diamond') {
+        document.getElementById(cardId.value).style.color = 'red'
+    } else {
+        document.getElementById(cardId.value).style.color = 'black'
+    }
+}
